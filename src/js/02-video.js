6215a616f40b215e1a96a3f178c7b5fcfbc6eeda
Olga Pozdniakova <nicktudan@gmail.com>
2022-12-15 23:56:02 +0100

    Done task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('timeupdate', throttle(event => {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(event.seconds));
+}, 1000) 
+);
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+
+player
+.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+.catch(function (error) {
+    console.error(error);
+});
